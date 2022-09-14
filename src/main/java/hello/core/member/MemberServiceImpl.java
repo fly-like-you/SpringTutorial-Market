4929eb56a25138c88b90e41bcab3039e96aff2d4
fly_like_you <qkrwnsgh71w@gmail.com>
2022-09-14 21:40:26 +0900

    컴포넌트 스캔 - 필터

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,11 +1,17 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
 
     // private final MemberRepository memberRepository = new MemoryMemberRepository();
     // 문제1 MemberRepository는 의존 하지만 MemoryMemberRepository는 구현체이기 때문에 DIP위배
     private final MemberRepository memberRepository;
+
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
