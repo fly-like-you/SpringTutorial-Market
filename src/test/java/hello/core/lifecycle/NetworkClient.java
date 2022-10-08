403e27d3b8ca09dc9b162371f18198f4b64fe675
fly_like_you <qkrwnsgh71w@gmail.com>
2022-10-08 22:56:15 +0900

    빈 생명 주기 - 빈 등록 초기화, 소멸 메서드

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,7 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
 
     public NetworkClient(){
@@ -21,24 +19,26 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     public void call(String message) {
-        System.out.println("call; = " + url + "message = " + message);
+        System.out.println("call = " + url + " message = " + message);
     }
 
     public void disconnect(){
         System.out.println("close: "+ url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+
+    public void init(){
         // 의존관계 주입이 끝나면 호출해주겠다.
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
 
     }
 
-    @Override
-    public void destroy() throws Exception {
+
+    public void close(){
         // 컨테이너 끝나기 전에 실행
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
